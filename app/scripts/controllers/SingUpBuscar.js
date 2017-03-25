d55f19dfc610c24a2e52b7aec150a1d1a9f3887f
Itzi Reyes <itzireyes@Itzis-MacBook-Pro.local>
2017-03-25 02:12:46 -0600

    Intento de botón en signupBUscarComedor

---
+++ app/scripts/controllers/SingUpBuscar.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTec1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTec1App
+ */
+
+
+
+myApp.controller('SignUpBuscarCtrl', function ($scope,$log,$http,$location) {
+     var  modelo={
+    };
+
+   $http({
+        method: 'GET',
+        url: '/comedor.json'
+        }).then(function successCallback(response) {
+            $log.debug("successCallback"+response.data.comedores);
+            $scope.modelo.comedores = response.data.comedores
+          }, function errorCallback(response) {
+            $log.debug("errorCallback");
+        });
+    $scope.modelo = modelo;
+   
+    $scope.getFav = function() {
+        //Colectar la informacion de arriba
+        $location.path('/favoritos');
+    }
+    
+  });
