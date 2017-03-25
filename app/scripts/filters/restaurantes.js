5d6b1046f699afbb8feae40f37a3560179f5cd53
Daniela Martin <daniela@esporaestudio.com>
2017-03-25 00:48:00 -0600

    Intentando crear filtro

--- app/scripts/filters/restaurantes.js
+++ app/scripts/filters/restaurantes.js
@@ -10,7 +10,16 @@
  */
 angular.module('claseAngularApp')
   .filter('restaurantes', function () {
-    return function (input) {
-      return 'restaurantes filter: ' + input;
-    };
+     return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+
   });
