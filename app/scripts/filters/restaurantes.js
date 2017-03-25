7316c40772ea5518d8d20c0ae51908c8e6ae9ab7
Daniela Martin <daniela@esporaestudio.com>
2017-03-25 02:20:55 -0600

    Se modificaron unas vistas

--- app/scripts/filters/restaurantes.js
+++ app/scripts/filters/restaurantes.js
@@ -10,7 +10,7 @@
  */
 angular.module('myApp')
   .filter('restaurantes', function () {
-  	return function(items, key, reverse) {
+  	return function(items, key, reverse, buttonF) {
     var resultado = [];
     angular.forEach(items, function(item) {
       resultado.push(item);
@@ -18,7 +18,7 @@ angular.module('myApp')
     resultado.sort(function (a, b) {
       return (a[key] > b[key] ? 1 : -1);
     });
-    if(reverse) resultado.reverse();
+    if(buttonF == true ) resultado.reverse();
     return resultado;
   };
 
--- app/scripts/services/shareddataservice.js
+++ app/scripts/services/shareddataservice.js
@@ -7,7 +7,7 @@
  * # SharedDataService
  * Service in the claseAngularApp.
  */
-angular.module('claseAngularApp')
+angular.module('myApp')
   .service('SharedDataService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
