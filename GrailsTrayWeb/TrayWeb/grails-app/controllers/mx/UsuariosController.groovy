6b318a682d8df5b46a044dae5f3d60774b246a2d
Daniela Martin <daniela@esporaestudio.com>
2017-05-03 09:27:22 -0500

    Se agrego la configuracion para la BD. Ya se agregan datos a la tabla alumnos de la bd trayweb.

---
+++ GrailsTrayWeb/TrayWeb/grails-app/controllers/mx/UsuariosController.groovy
@@ -0,0 +1,12 @@
+package mx
+
+import grails.rest.*
+import grails.converters.*
+
+class UsuariosController extends RestfulController {
+	static responseFormats = ['json', 'xml']
+	UsuariosController(){
+		super(Usuarios)
+	}
+    def index() { }
+}
--- GrailsTrayWeb/TrayWeb/grails-app/init/trayweb/BootStrap.groovy
+++ GrailsTrayWeb/TrayWeb/grails-app/init/trayweb/BootStrap.groovy
@@ -1,8 +1,16 @@
-package trayweb
+package TrayWeb
+import mx.*
 
 class BootStrap {
 
     def init = { servletContext ->
+    	new Usuarios(nombre: "Daniela",
+    				apellido: "Martin",
+    				telefono: "5523899177",
+    				email: "dani@mail.com",
+    				nickname: "dani",
+    				edad:"30",
+    				password: "123456").save()
     }
     def destroy = {
     }
