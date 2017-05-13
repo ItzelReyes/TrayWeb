cf8631f8020c19887c5b566584f86cad4d7bead6
Itzi Reyes <itzireyes11@gmail.com>
2017-05-12 21:15:08 -0500

    Ya funciona post con boolean

--- GrailsTrayWeb/TrayWeb/grails-app/domain/mx/PrefDiarias.groovy
+++ GrailsTrayWeb/TrayWeb/grails-app/domain/mx/PrefDiarias.groovy
@@ -4,19 +4,19 @@ import grails.rest.*
 @Resource(readOnly = false, formats = ['json', 'xml'])
 class PrefDiarias {
 
-    Date fecha = new Date()
+    String fecha
     String hora
-	boolean res
-	boolean pescado
-	boolean pollo
-	boolean vegetariano
+    boolean res
+    boolean pescado
+    boolean pollo
+    boolean vegetariano
 
     static constraints = {
         fecha blank:false
         hora blank:false
-    	res defaultValue:false
-    	pescado defaultValue:false
-    	pollo defaultValue:false
-    	vegetariano defaultValue:false
+        res defaultValue:false
+        pescado defaultValue:false
+        pollo defaultValue:false
+        vegetariano defaultValue:false
     }
 }
